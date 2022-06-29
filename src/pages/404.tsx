1abec5664593f349dcd06a0ff108ac93e3d8b238
Luis Marsiglia <marsiglia.business@gmail.com>
2022-06-28 23:16:42 -0400

    perf: :zap: fix go back home button

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
-import { Button, Heading, Text, useColorModeValue } from '@chakra-ui/react'
+import { Heading, Text, useColorModeValue } from '@chakra-ui/react'
 import Layout from '@layouts/main'
 import { Link } from '@components'
 
 const Custom404: NextPage = () => {
   const textColor = useColorModeValue('gray.600', 'gray.400')
+
+  const buttonFg = useColorModeValue('white', 'white')
+  const buttonBg = useColorModeValue('teal.500', 'teal.400')
+  const onHoverButtonBg = useColorModeValue('teal.600', 'teal.500')
 
   return (
     <Layout title='404'>
@@ -13,17 +17,25 @@ const Custom404: NextPage = () => {
       </Heading>
 
       <Text color={textColor} mb={8}>
-        {
-          `Why show a generic 404 when I can make it sound mysterious? It seems
+        {`Why show a generic 404 when I can make it sound mysterious? It seems
           you've found something that used to exist, or you spelled something
           wrong. I'm guessing you spelled something wrong. Can you double
-          check that URL?`
-        }
+          check that URL?`}
       </Text>
 
-      <Button as={Link} colorScheme='teal' href='/' w={48}>
+      <Link
+        _hover={{ bg: onHoverButtonBg }}
+        bg={buttonBg}
+        color={buttonFg}
+        fontSize={['sm', 'md']}
+        href='/'
+        px={4}
+        py={2}
+        rounded='lg'
+        // w={48}
+      >
         Return to home
-      </Button>
+      </Link>
     </Layout>
   )
 }
