48bf21dc077b4537b7c1a280fb515e725bf8ca5e
Luis Marsiglia <marsiglia.business@gmail.com>
2022-07-25 23:55:49 -0400

    chore: :arrow_up: upgrade deps, eslint config and next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    dirs: ['src']
-  },
-  reactStrictMode: false,
-  serverRuntimeConfig: {
-    PROJECT_ROOT: __dirname
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
