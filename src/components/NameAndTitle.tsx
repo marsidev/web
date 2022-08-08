12c2bdcbca32385593dfc7c7e5130e5709b6f31a
Luis Marsiglia <marsiglia.business@gmail.com>
2022-08-08 18:46:09 -0400

    fix: fix responsive issue

--- src/components/NameAndTitle.tsx
+++ src/components/NameAndTitle.tsx
@@ -1,17 +1,23 @@
-import { Box, Heading, chakra } from '@chakra-ui/react'
+import { Heading, Stack, chakra } from '@chakra-ui/react'
 
 export const NameAndTitle = () => {
 	return (
-		<Box>
-			<Heading as='h1' fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+		<Stack alignItems={{ base: 'center', md: 'flex-start' }} direction='column'>
+			<Heading
+				as='h1'
+				fontSize={{ base: '2xl', sm: '3xl', md: '4xl', lg: '5xl' }}
+			>
 				{'Hi, I\'m '}
 				<chakra.span>{'Luis Marsiglia'}</chakra.span>
 				<chakra.span className='hand'>👋</chakra.span>
 			</Heading>
 
-			<Heading as='h2' fontSize={{ base: 'xl', md: '2xl', lg: '3xl' }}>
+			<Heading
+				as='h2'
+				fontSize={{ base: 'xl', sm: '2xl', md: '2xl', lg: '3xl' }}
+			>
 				{'Front-end developer.'}
 			</Heading>
-		</Box>
+		</Stack>
 	)
 }
