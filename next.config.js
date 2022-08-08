58e3a0e8d838e8c3867035277e5f17c714e06905
Luis Marsiglia <marsiglia.business@gmail.com>
2022-08-08 18:30:23 -0400

    chore: add swc options

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+	reactStrictMode: true,
+	compiler: {
+		removeConsole: process.env.NODE_ENV === 'production'
+	}
 }
 
 module.exports = nextConfig
