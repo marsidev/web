b39fa8f0c88c6ae6fc385451d69cdaeff8ca0e29
Luis Marsiglia <marsiglia.business@gmail.com>
2022-08-10 23:37:10 -0400

    feat: add under construction banner

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, useColorModeValue } from '@chakra-ui/react'
 import { DefaultSeo } from 'next-seo'
 import { useSuperState } from '@superstate/react'
 import { Navbar } from '~/components'
@@ -14,12 +14,24 @@ interface LayoutProps {
 export const Layout: React.FC<LayoutProps> = ({ children, title }) => {
 	useSuperState(mobileMenu.state)
 	const menuExpanded = mobileMenu.get()
+	const gradient = useColorModeValue(
+		'linear(to bottom right, red.500, pink.600)',
+		'linear(to bottom right, red.400, pink.500)'
+	)
 
 	return (
 		<>
 			<DefaultSeo title={title} {...seo} />
 
 			<Navbar minH={['8vh', '10vh']} />
+
+			<Flex
+				bgGradient={gradient}
+				color={useColorModeValue('white', 'black')}
+				justify='center'
+			>
+				This site is a work in progress.
+			</Flex>
 
 			<Flex
 				as='main'
