297d3d8889b9c3accdf8df24f4739075cad5d65d
Luis Marsiglia <marsiglia.business@gmail.com>
2022-08-14 13:02:47 -0400

    feat: add tracking tools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,15 @@ import 'atropos/css'
 import type { AppType } from 'next/dist/shared/lib/utils'
 import { AnimatePresence } from 'framer-motion'
 import { ChakraProvider } from '@chakra-ui/react'
+import splitbee from '@splitbee/web'
+import { usePanelbear } from '@panelbear/panelbear-nextjs'
 import { theme } from '~/theme'
 import { ScrollToTop } from '~/components'
-// import { usePanelbear } from '@panelbear/panelbear-nextjs'
-// import splitbee from '@splitbee/web'
-// process.env.NODE_ENV === 'production' && splitbee.init()
+
+process.env.NODE_ENV === 'production' && splitbee.init()
 
 const MyApp: AppType = ({ Component, pageProps, router }) => {
+	usePanelbear('IO32G8weAys')
 	return (
 		<ChakraProvider theme={theme}>
 			<AnimatePresence exitBeforeEnter>
