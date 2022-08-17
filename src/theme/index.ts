b8266d1330b8cdf893fea06dab5745f2087c219f
Luis Marsiglia <marsiglia.business@gmail.com>
2022-08-16 21:11:49 -0400

    feat: change outline focus color

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -8,9 +8,17 @@ export const config: ThemeConfig = {
 	useSystemColorMode: false
 }
 
+/* this replace all outline focus color excluding input,
+	see https://github.com/chakra-ui/chakra-ui/discussions/2459#discussioncomment-2412881
+*/
+const shadows = {
+	outline: '0 0 0 3px var(--chakra-colors-teal-400)'
+}
+
 export const theme = extendTheme({
 	config,
 	fonts,
 	components,
-	styles
+	styles,
+	shadows
 })
