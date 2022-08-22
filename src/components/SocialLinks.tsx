5ccac5b529fbc279244460a99b440d525113c1b4
Luis Marsiglia <marsiglia.business@gmail.com>
2022-08-22 19:31:38 -0400

    feat: add alt text to social links

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,4 +1,4 @@
-import { Stack, type StackProps, useColorModeValue } from '@chakra-ui/react'
+import { Stack, type StackProps, VisuallyHidden, useColorModeValue } from '@chakra-ui/react'
 import { EmailIcon, GithubIcon, LinkedInIcon, TwitterIcon } from '~/icons'
 import { Link } from '~/components'
 import { SOCIAL_LINKS } from '~/constants'
@@ -6,12 +6,19 @@ import { SOCIAL_LINKS } from '~/constants'
 interface SocialIconProps {
 	href: string
 	icon: React.ReactNode
+	alt: string
 }
 
-const SocialIcon: React.FC<SocialIconProps> = ({ href, icon }) => {
+const SocialIcon: React.FC<SocialIconProps> = ({ href, icon, alt }) => {
 	const onHoverColor = useColorModeValue('pink.600', 'pink.500')
 	return (
-		<Link isExternal _hover={{ color: onHoverColor }} borderRadius='md' href={href}>
+		<Link
+			isExternal
+			_hover={{ color: onHoverColor }}
+			borderRadius='md'
+			href={href}
+		>
+			<VisuallyHidden>{alt}</VisuallyHidden>
 			{icon}
 		</Link>
 	)
@@ -26,21 +33,25 @@ export const SocialLinks: React.FC<StackProps> = props => {
 			{...props}
 		>
 			<SocialIcon
+				alt='GitHub profile link'
 				href={SOCIAL_LINKS.github}
 				icon={<GithubIcon className='social-link' />}
 			/>
 
 			<SocialIcon
+				alt='LinkedIn profile link'
 				href={SOCIAL_LINKS.linkedin}
 				icon={<LinkedInIcon className='social-link' />}
 			/>
 
 			<SocialIcon
+				alt='Twitter profile link'
 				href={SOCIAL_LINKS.twitter}
 				icon={<TwitterIcon className='social-link' />}
 			/>
 
 			<SocialIcon
+				alt='Compose email link'
 				href={SOCIAL_LINKS.email}
 				icon={<EmailIcon className='social-link' />}
 			/>
