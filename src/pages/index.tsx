f6e147f34a99a5a1775d44ca1a2dcd833b3fe961
Luis Marsiglia <marsiglia.business@gmail.com>
2022-08-22 22:09:38 -0400

    perf: render projects with lazy load

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,23 @@
 import type { InferGetServerSidePropsType, NextPage } from 'next'
 import { serialize } from 'next-mdx-remote/serialize'
+import { Suspense, lazy } from 'react'
 import { Layout } from '~/layouts/main'
 import { loadFile } from '~/utils/fs'
-import { About, Cover, Projects } from '~/views'
+import { About, Cover } from '~/views'
 
 type AppProps = InferGetServerSidePropsType<typeof getStaticProps>
+
+const Projects = lazy(() => import('~/views/Projects'))
 
 const App: NextPage<AppProps> = ({ aboutSource }) => {
 	return (
 		<Layout>
 			<Cover />
 			<About source={aboutSource} />
-			<Projects />
+
+			<Suspense fallback='loading...'>
+				<Projects />
+			</Suspense>
 		</Layout>
 	)
 }
