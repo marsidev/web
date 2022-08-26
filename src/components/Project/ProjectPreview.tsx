dd15c4b68994e96653cbd06a7f745e6182cb6936
Luis Marsiglia <marsiglia.business@gmail.com>
2022-08-26 14:41:40 -0400

    feat: add skeleton placeholder to project previews

--- src/components/Project/ProjectPreview.tsx
+++ src/components/Project/ProjectPreview.tsx
@@ -1,10 +1,12 @@
 import {
 	Flex,
 	type FlexProps,
+	Skeleton,
 	useBreakpointValue,
 	useColorModeValue
 } from '@chakra-ui/react'
 import { Atropos } from 'atropos/react'
+import { useState } from 'react'
 import { Project } from '~/types'
 import { CloudinaryImage } from '~/components'
 
@@ -19,6 +21,9 @@ export const ProjectPreview: React.FC<ProjectProps> = ({
 	const bg = useColorModeValue('gray.100', 'gray.700')
 	const borderColor = useColorModeValue('gray.200', 'gray.600')
 	const previewPadding = useBreakpointValue({ base: '1em', sm: '1.6em', md: '2em' })
+	const [desktopLoaded, setDesktopLoaded] = useState<boolean>(false)
+	const [mobileLoaded, setMobileLoaded] = useState<boolean>(false)
+	const loaded = desktopLoaded && mobileLoaded
 
 	return (
 		<Flex
@@ -30,34 +35,42 @@ export const ProjectPreview: React.FC<ProjectProps> = ({
 			pos='relative'
 			{...props}
 		>
-			<Atropos
-				className='project-preview'
-				rotateTouch='scroll-y'
-				rotateXMax={24}
-				rotateYMax={24}
-				shadow={false}
+			<Skeleton
+				borderRadius={{ base: 8, md: 16 }}
+				fadeDuration={1}
+				isLoaded={loaded}
 			>
-				<CloudinaryImage
-					alt={`${p.name} desktop preview`}
-					className='project-preview-desktop'
-					data-atropos-offset='-4'
-					lazyLoadPlugin={true}
-					placeholderPlugin={true}
-					publicId={`marsidev${p.images!.desktop}`}
-					style={{
-						padding: previewPadding
-					}}
-				/>
+				<Atropos
+					className='project-preview'
+					rotateTouch='scroll-y'
+					rotateXMax={24}
+					rotateYMax={24}
+					shadow={false}
+				>
+					<CloudinaryImage
+						alt={`${p.name} desktop preview`}
+						className='project-preview-desktop'
+						data-atropos-offset='-4'
+						lazyLoadPlugin={true}
+						placeholderPlugin={true}
+						publicId={`marsidev${p.images!.desktop}`}
+						style={{
+							padding: previewPadding
+						}}
+						onLoad={() => setDesktopLoaded(true)}
+					/>
 
-				<CloudinaryImage
-					alt={`${p.name} mobile preview`}
-					className='project-preview-mobile'
-					data-atropos-offset='8'
-					lazyLoadPlugin={true}
-					placeholderPlugin={true}
-					publicId={`marsidev${p.images!.mobile}`}
-				/>
-			</Atropos>
+					<CloudinaryImage
+						alt={`${p.name} mobile preview`}
+						className='project-preview-mobile'
+						data-atropos-offset='8'
+						lazyLoadPlugin={true}
+						placeholderPlugin={true}
+						publicId={`marsidev${p.images!.mobile}`}
+						onLoad={() => setMobileLoaded(true)}
+					/>
+				</Atropos>
+			</Skeleton>
 		</Flex>
 	)
 }
