0319f87f925e44f1eb9e8b8d1655a20c17a1ce88
Luis Marsiglia <marsiglia.business@gmail.com>
2022-09-05 20:08:06 -0400

    perf: use forwardRef in Link component

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,14 +1,17 @@
-import { Link as ChakraLink, type LinkProps as ChakraLinkProps } from '@chakra-ui/react'
-import NextLink, { type LinkProps as NextLinkProps } from 'next/link'
+import type { LinkProps as NextLinkProps } from 'next/link'
+import type { FC } from 'react'
+import type { LinkProps as ChakraLinkProps } from '@chakra-ui/react'
+import { Link as ChakraLink, forwardRef } from '@chakra-ui/react'
+import NextLink from 'next/link'
 
 export type LinkProps = ChakraLinkProps & NextLinkProps
 
-export const Link: React.FC<LinkProps> = ({ href, children, ...props }) => (
+export const Link: FC<LinkProps> = forwardRef(({ href, children, ...props }, ref) => (
 	<NextLink passHref href={href}>
-		<ChakraLink {...props}>
+		<ChakraLink ref={ref} {...props}>
 			{children}
 		</ChakraLink>
 	</NextLink>
-)
+))
 
 export default Link
