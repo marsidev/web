39e16f4d7dc8382cb177d736cfceaf436bcd5c34
Luis Marsiglia <marsiglia.business@gmail.com>
2022-09-07 22:08:45 -0400

    fix: fix 100vh mobile issue

--- src/views/Cover.tsx
+++ src/views/Cover.tsx
@@ -1,6 +1,7 @@
 import { Flex, Stack, forwardRef } from '@chakra-ui/react'
 import { useAtom } from 'jotai'
 import { useCallback } from 'react'
+import { use100vh } from 'react-div-100vh'
 import { Avatar } from '~/components/Avatar'
 import { NameAndTitle } from '~/components/NameAndTitle'
 import { SocialLinks } from '~/components/SocialLinks'
@@ -8,13 +9,16 @@ import { navbarHeightAtom } from '~/store'
 import { chakraUnitToPx } from '~/utils/units'
 
 const layoutTopPadding = 8 // em - chakra units
+const layoutPxPadding = chakraUnitToPx(layoutTopPadding)
 
 export const Cover = forwardRef((props, ref) => {
 	const [navbarHeight] = useAtom(navbarHeightAtom)
+	const _100vh = use100vh()
+	const windowHeight = _100vh ? `${_100vh}px` : '100vh'
 
 	const getCoverHeight = useCallback(() => {
-		// pageHeight - navbarHeight - topPadding
-		return `calc(100vh - ${navbarHeight}px - ${chakraUnitToPx(layoutTopPadding)}px)`
+		// windowHeight - navbarHeight - topPadding
+		return `calc(${windowHeight} - ${navbarHeight}px - ${layoutPxPadding}px)`
 	}, [navbarHeight, layoutTopPadding])
 
 	return (
@@ -25,8 +29,8 @@ export const Cover = forwardRef((props, ref) => {
 			direction='column'
 			height={getCoverHeight()}
 			id='cover'
-			justify={{ base: 'flex-start', sm: 'center' }}
-			pt={{ base: 32, sm: 0 }}
+			justify='center'
+			pt={0}
 			w='full'
 			{...props}
 		>
