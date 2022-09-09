d7aa8de9428b439f99a4206d84a207ad00e933fa
Luis Marsiglia <marsiglia.business@gmail.com>
2022-09-09 02:21:13 -0400

    perf: render projects with lazy behavior by using react-intersection-observer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { InferGetServerSidePropsType, NextPage } from 'next'
 import { serialize } from 'next-mdx-remote/serialize'
+import { useInView } from 'react-intersection-observer'
 import { Layout } from '~/layouts/main'
 import { loadFile } from '~/utils/fs'
 import { Cover } from '~/views/Cover'
 import { About } from '~/views/About'
+import { LazyRender } from '~/components/LazyRender'
 import { Projects } from '~/views/Projects'
 import { useActiveSection } from '~/hooks/use-active-section'
 
@@ -12,11 +14,23 @@ type AppProps = InferGetServerSidePropsType<typeof getStaticProps>
 const App: NextPage<AppProps> = ({ aboutSource }) => {
 	const { aboutRef, coverRef, projectsRef } = useActiveSection()
 
+	const { ref: aboutParentRef, inView: aboutSectionShowedEnough } =
+		useInView({
+			threshold: 0.5,
+			triggerOnce: true
+		})
+
 	return (
 		<Layout>
 			<Cover ref={coverRef} />
-			<About ref={aboutRef} source={aboutSource} />
-			<Projects ref={projectsRef} />
+
+			<div ref={aboutParentRef}>
+				<About ref={aboutRef} source={aboutSource} />
+			</div>
+
+			<LazyRender shouldRender={aboutSectionShowedEnough}>
+				<Projects ref={projectsRef} />
+			</LazyRender>
 		</Layout>
 	)
 }
