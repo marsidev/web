77321b02488109c0bc5813ffe6d4504f32630310
Luis Marsiglia <marsiglia.business@gmail.com>
2022-10-19 23:45:42 -0400

    fix layout issue

--- src/components/Project/PackagePreview.tsx
+++ src/components/Project/PackagePreview.tsx
@@ -1,61 +1,69 @@
 import { Skeleton } from '@chakra-ui/skeleton'
-import { Flex, Heading } from '@chakra-ui/layout'
-import { useCallback, useState } from 'react'
+import { Box, Flex, type FlexProps, Heading } from '@chakra-ui/layout'
+import { useCallback, useMemo, useState } from 'react'
 import { useBreakpointValue } from '@chakra-ui/media-query'
 import type { Project } from '~/types'
 import { CloudinaryImage } from '~/components/CloudinaryImage'
 import { Link } from '~/components/Link'
 
-interface PackagePreviewProps {
+interface PackagePreviewProps extends FlexProps {
 	project: Project
+	placeholderHeight: number | undefined
+	imageWidth: number
 }
 
 export const PackagePreview: React.FC<PackagePreviewProps> = props => {
-	const { project } = props
+	const { project, placeholderHeight, imageWidth, ...rest } = props
 	const [imageLoaded, setImageLoaded] = useState(false)
-	const onLoad = useCallback(() => setImageLoaded(true), [])
 	const borderRadius = useBreakpointValue({ base: '8px', md: '16px' }, { fallback: 'base' })
-	const cardHeight = useBreakpointValue({ base: 140, md: 240 }, { fallback: 'base' })
-	const nameHeight = useBreakpointValue({ base: 40, md: 60 }, { fallback: 'base' })
+
+	const onLoad = useCallback(() => setImageLoaded(true), [])
+	const imageHeight = useMemo(() => Math.floor(imageWidth * 0.5), [imageWidth])
 
 	return (
-		<Skeleton
-			borderRadius={borderRadius}
-			fadeDuration={1}
-			height={cardHeight! + nameHeight!}
-			isLoaded={imageLoaded}
-			width='full'
-		>
-			<Link isExternal href={project.url!}>
-				<CloudinaryImage
-					useNextImageInDevelopment
-					alt='npm'
-					deliveryHeight={600}
-					deliveryQuality={80}
-					deliveryWidth={1200}
-					height={cardHeight!}
-					lazyLoadPlugin={true}
-					loading='lazy'
-					placeholderPlugin={true}
-					src='/images/bg/npm.png'
-					style={{
-						borderStartStartRadius: borderRadius,
-						borderStartEndRadius: borderRadius,
-						objectFit: 'cover',
-						width: '600px',
-						height: `${cardHeight}px`
-					}}
-					subPath='marsidev'
-					width={600}
-					onLoad={onLoad}
-				/>
+		<Flex align='center' height={placeholderHeight} width='full' {...rest}>
+			<Skeleton
+				borderRadius={borderRadius}
+				fadeDuration={1}
+				height={placeholderHeight}
+				isLoaded={imageLoaded}
+				width='full'
+			>
+				<Link isExternal h='full' href={project.url!}>
+					<Flex flexDir='column' h='full'>
+						<Box h='80%'>
+							<CloudinaryImage
+								useNextImageInDevelopment
+								alt='npm'
+								deliveryHeight={600}
+								deliveryQuality={75}
+								deliveryWidth={1200}
+								height={imageHeight}
+								lazyLoadPlugin={true}
+								loading='lazy'
+								placeholderPlugin={true}
+								src='/images/bg/npm.png'
+								style={{
+									borderStartStartRadius: borderRadius,
+									borderStartEndRadius: borderRadius,
+									objectFit: 'cover',
+									width: '100%',
+									height: placeholderHeight ? `${placeholderHeight * 0.8}px` : undefined
+								}}
+								subPath='marsidev'
+								width={imageWidth}
+								onLoad={onLoad}
+							/>
+						</Box>
 
-				<Flex align='center' minH={`${nameHeight}px`} p={{ base: 2, md: 4 }}>
-					<Heading as='h4' fontSize='md' textAlign='left'>
-						{project.packageName!}
-					</Heading>
-				</Flex>
-			</Link>
-		</Skeleton>
+						<Flex align='center' h='20%' p={{ base: 2, md: 4 }}>
+							<Heading as='h4' fontSize='md' textAlign='left'>
+								{project.packageName!}
+							</Heading>
+						</Flex>
+					</Flex>
+				</Link>
+			</Skeleton>
+		</Flex>
 	)
 }
--- src/components/Project/ProjectPreview.tsx
+++ src/components/Project/ProjectPreview.tsx
@@ -1,7 +1,7 @@
 import { useBreakpointValue } from '@chakra-ui/media-query'
 import { useColorModeValue } from '@chakra-ui/system'
 import { Flex, type FlexProps } from '@chakra-ui/layout'
-import { type FC, useEffect, useRef, useState } from 'react'
+import { type FC, useEffect, useMemo, useRef, useState } from 'react'
 import type { Project } from '~/types'
 import { remToPx } from '~/utils/units'
 import { defaultDesktopSize, defaultMobileSize, getContainerHeight, getDimensions } from '~/utils/project-preview'
@@ -22,21 +22,30 @@ export const ProjectPreview: FC<ProjectProps> = ({ project: p, ...props }) => {
 	const [containerWidth, setContainerWidth] = useState<number | null>(null)
 	const previewPadding = useBreakpointValue({ base: '1rem', sm: '1.6rem', md: '2rem' }, { fallback: 'base' })
 
-	const containerMinHeight = containerWidth ? getContainerHeight(containerWidth) : undefined
-	const placeholderHeight = containerMinHeight ? containerMinHeight * 0.8 : undefined
-
 	const hasScreenshots = p.images?.desktop && p.images?.mobile
 	const isNpmLib = !hasScreenshots && p.tags.includes('package') && p.stack.includes('npm') && p.url && p.packageName
+
+	const containerMinHeight = useMemo(() => {
+		return containerWidth
+			? isNpmLib
+				? Math.floor(containerWidth * 0.4 + remToPx(2))
+				: getContainerHeight(containerWidth)
+			: undefined
+	}, [containerWidth, isNpmLib, containerWidth])
+
+	const placeholderHeight = useMemo(() => {
+		return containerMinHeight ? (isNpmLib ? containerMinHeight : containerMinHeight * 0.8) : undefined
+	}, [isNpmLib, containerMinHeight])
 
 	useEffect(
 		function setSizes() {
 			if (containerRef.current) {
 				const containerWidth = containerRef.current.clientWidth
-				const desktopPadding = 2 * remToPx(parseInt(previewPadding!))
+				setContainerWidth(containerWidth)
 
+				const desktopPadding = 2 * remToPx(parseInt(previewPadding!))
 				const { desktop: desktopSize, mobile: mobileSize } = getDimensions(containerWidth, desktopPadding)
 
-				setContainerWidth(containerWidth)
 				setDesktopSize(desktopSize)
 				setMobileSize(mobileSize)
 			}
@@ -57,7 +66,7 @@ export const ProjectPreview: FC<ProjectProps> = ({ project: p, ...props }) => {
 			borderColor={borderColor}
 			borderRadius={{ base: 8, md: 16 }}
 			justify='center'
-			minHeight={isNpmLib ? 180 : containerMinHeight}
+			minHeight={containerMinHeight}
 			pos='relative'
 			width='full'
 			{...props}
@@ -71,7 +80,7 @@ export const ProjectPreview: FC<ProjectProps> = ({ project: p, ...props }) => {
 				/>
 			)}
 
-			{isNpmLib && <PackagePreview project={p} />}
+			{isNpmLib && <PackagePreview imageWidth={desktopSize.width} placeholderHeight={placeholderHeight} project={p} />}
 		</Flex>
 	)
 }
